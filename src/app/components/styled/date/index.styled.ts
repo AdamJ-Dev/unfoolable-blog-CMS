0dc11b232e073ece266b19e760be1dab79216123
AdamJ-Dev <adam.g.johnson@hotmail.co.uk>
2022-09-15 08:22:43 +0100

    feat: getDateDisplay for blogs

---
+++ src/app/components/styled/date/index.styled.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Date = styled.span`
+  font-size: 15px;
+  color: gray;
+`;
+
+export default Date;
--- src/app/pages/blogs/index.tsx
+++ src/app/pages/blogs/index.tsx
@@ -1,7 +1,10 @@
+import type { Blog } from '../../types/blogs';
 import Container from '../../components/styled/container/index.styled';
 import Expander from '../../components/widgety/expander';
 import BlogActions from './blog-actions';
-import type { Blog } from '../../utility/blogs/types';
+import styles from './index.module.css';
+import { getDateDisplay } from '../../../lib/format/get-date-display';
+import Date from '../../components/styled/date/index.styled';
 
 type BlogsPageProps = {
   blogs: Blog[];
@@ -10,11 +13,19 @@ type BlogsPageProps = {
 const BlogsPage: React.FC<BlogsPageProps> = ({ blogs }) => {
   return (
     <Container>
-      <h1>Posts</h1>
-      <ul>
+      <h1>Blogs</h1>
+      <ul className={styles.blogsList}>
         {blogs.map((blog) => (
           <li key={blog._id}>
-            <Expander target={<span>{blog.title}</span>} details={<BlogActions id={blog._id} />} />
+            <Expander
+              target={
+                <span className={styles.blogInfo}>
+                  <span className={styles.blogTitle}>{blog.title} </span>
+                  <Date>({getDateDisplay(blog.createdAt, blog.updatedAtDate)})</Date>
+                </span>
+              }
+              details={<BlogActions id={blog._id} />}
+            />
           </li>
         ))}
       </ul>
