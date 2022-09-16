db6747b46901fab4a62f5e6f9fa9e886ce3832f3
AdamJ-Dev <adam.g.johnson@hotmail.co.uk>
2022-09-16 13:01:20 +0100

    style: more general block/container styled components

--- src/app/components/styled/block/index.styled.ts
+++ src/app/components/styled/block/index.styled.ts
@@ -1,12 +1,17 @@
 import styled from 'styled-components';
 
-const Block = styled.div`
+type BlockProps = {
+  m?: string | number;
+  p?: string | number;
+};
+
+const Block = styled.div<BlockProps>`
   display: block;
   background: ${(props) => {
     return props.color || props.theme.colours.block;
   }};
-  padding: 15px;
-  margin: 15px 0;
+  margin: ${({ m }) => m || '15px 0'};
+  padding: ${({ p }) => p || '15px'};
 `;
 
 export default Block;
